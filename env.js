b402dcbeef9f52213aade66803bfee1e258cdb2a
cafeSowoo <harminis@gmail.com>
2026-05-29 20:17:14 +0900

    Require Supabase auth for local preview

--- env.js
+++ env.js
@@ -3,5 +3,5 @@ window.CODEX_HEALTH_CONFIG = {
   supabaseAnonKey: "sb_publishable_26z0I3bqyus87hZ7nLUekQ_DxVf8R3G",
   siteUrl: "http://localhost:4177/",
   ownerEmail: "harminis@gmail.com",
-  codexLocalPreviewBypassAuth: true
+  codexLocalPreviewBypassAuth: false
 };
